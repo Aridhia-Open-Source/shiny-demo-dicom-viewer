5f718f697ad4ccaeb8e4d8935d52d40a4a2613e7
Harry Peaker <HarryPeaker@Aridhia.net>
2019-02-12 11:27:07 +0000

    update datafile path

--- workspace/mini-apps/image_analysis/global.R
+++ workspace/mini-apps/image_analysis/global.R
@@ -4,12 +4,11 @@ xap.require("oro.dicom", "oro.nifti", "RPostgreSQL", "purrr")
 
 if(exists("xap.conn")) {
   home <- "~"
-  datafile_path <- "datafiles/uploads/DICOM Images/"
 } else {
   home <- workspace_path()
-  datafile_path <- "datafiles/DICOM Images/"
 }
 
+datafile_path <- "datafiles/DICOM Images/"
 
 writeTable <- function (df, tablename) {
   dbWriteTable(xap.conn, c(xap.db.sandbox, tablename), as.data.frame(df),
