77c78429ae12f26a0de2a36a7c40917404d0082b
Harry Peaker <HarryPeaker@Aridhia.net>
2021-01-05 15:05:10 +0000

    restructure

--- global.R
+++ global.R
@@ -1,20 +1,16 @@
 
-xap.require("oro.dicom", "oro.nifti", "RPostgreSQL", "purrr")
+library(oro.dicom)
+library(oro.nifti)
+library(RPostgreSQL)
+library(purrr)
 
-
-if(exists("xap.conn")) {
-  home <- "~"
-} else {
-  home <- workspace_path()
-}
-
-datafile_path <- "datafiles/DICOM Images/"
+datafile_path <- "data/dicom_images/"
 
 writeTable <- function (df, tablename) {
   dbWriteTable(xap.conn, c(xap.db.sandbox, tablename), as.data.frame(df),
                row.names = F, overwrite = F, append = TRUE)
 }
 
-pth <- file.path(home, datafile_path)
+pth <- datafile_path
 dirs <- list.dirs(path = pth, full.names = FALSE, recursive = TRUE)
 default_dicom <- dirs[2]
