1c704a758cbb4a03c4a20d042bd1fd830578fcad
annadomenech <anna.domenech@aridhia.com>
2021-02-05 16:40:14 +0000

    added video

--- code/help_tab.R
+++ code/help_tab.R
@@ -8,13 +8,11 @@ documentation_tab <- function() {
                          imaging information and related data."),
                        p("The sample images displaying the app are results from Magnetic Resonances of the brain. 
                          The source of this data is: http://www.osirix-viewer.com/datasets/DATA/BRAINIX.zip"),
-                       h4("Mini-app layout"),
-                       p("This help tab will gives you an overview of the mini-app itself, the other tab called 'DICOM Viewer' is the app itself."),
                        h4("Adding your images"),
                        p("To use other DICOM images in the app, you can place sets of DICOM images 
                          inside the folder called 'dicom_images' within the 'data' folder in the parent directory" ),
                        h4("Using the App"),
-                       p("You can select the patient file to display from the dropdown menu on the left. Then you can change the Orientation of the 
+                       p("You can select the patient file to display from the drop-down menu on the left. Then you can change the Orientation of the 
                          image being displayed using the sliders"),
                        p("In the middle of the screen the image will be displayed. You can change the plane view by clicking on 'Axial', 'Sagittal' or 
                          'Coronal' tabs just above the image. Under the image a table with the file information is printed."),
@@ -24,7 +22,7 @@ documentation_tab <- function() {
                      column(
                        6,
                        h3("Walkthrough video"),
-                       tags$video(src="survival.mp4", type = "video/mp4", width="100%", height = "350", frameborder = "0", controls = NA),
+                       tags$video(src="dicom-viewer.mp4", type = "video/mp4", width="100%", height = "350", frameborder = "0", controls = NA),
                        p(class = "nb", "NB: This mini-app is for provided for demonstration purposes, is unsupported and is utilised at user's 
                        risk. If you plan to use this mini-app to inform your study, please review the code and ensure you are 
                        comfortable with the calculations made before proceeding. ")
